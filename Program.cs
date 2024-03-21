7cf63a002bcf2e8a16759bd63827e843a2d157ca
BorislavZh <borislavpaypal123@gmail.com>
2024-03-21 23:18:13 +0200

    най накрая

--- Program.cs
+++ Program.cs
@@ -6,21 +6,19 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+// Add DbContext Configuration
+// Ensure you have a connection string named "DefaultConnectionString" in your appsettings.json
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString")));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
-//DbContext Configuration
-builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString")));
-
-
-
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
