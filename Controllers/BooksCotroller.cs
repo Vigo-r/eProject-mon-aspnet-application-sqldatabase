6050eddc208f0d9c6ae10315b7b9d59c0a0878dc
VIgo-r <prazno.kalin@gmail.com>
2024-03-22 19:56:26 +0200

    Additional Controllers - Libraries - fixed, Books - added.

---
+++ Controllers/BooksCotroller.cs
@@ -0,0 +1,21 @@
+﻿using eTickets.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace eTickets.Controllers
+{
+    public class BooksCotroller : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public BooksCotroller (AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var allBooks = await _context.Books.ToListAsync();
+            return View();
+        }
+    }
+}
