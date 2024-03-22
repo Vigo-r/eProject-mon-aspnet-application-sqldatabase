b85986e0b5045ab3f609d65814b5f07310a95512
VIgo-r <prazno.kalin@gmail.com>
2024-03-22 19:23:52 +0200

    Some, controllers

---
+++ Controllers/PublishersController.cs
@@ -0,0 +1,21 @@
+﻿using eTickets.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace eTickets.Controllers
+{
+    public class PublishersController : Controller
+    {
+        private readonly AppDbContext _cotext;
+        public PublishersController(AppDbContext context)
+        {
+            _cotext = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var allPublishers= await _cotext.Publishers.ToListAsync();
+
+            return View();
+        }
+    }
+}
