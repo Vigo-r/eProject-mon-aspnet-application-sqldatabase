adb733e74ab9022bfee31f8eb541e9d9b3c1df9b
VIgo-r <prazno.kalin@gmail.com>
2024-03-23 14:23:55 +0200

    Services, early stage, jut to synchronize master branch.

---
+++ Data/Services/AuthorsService.cs
@@ -0,0 +1,57 @@
+﻿using eTickets.Models;
+
+namespace eTickets.Data.Services
+{
+    public class AuthorsService : IAuthorsService
+    {
+        public void Add(Author author)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task AddAsync(Author entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Author> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Author>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Author GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Author> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Author Update(int id, Author newAuthor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(int id, Author entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
