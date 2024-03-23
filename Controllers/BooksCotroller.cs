28888949cbd87d3a0a6e3500f03643ae24724054
BorislavZh <borislavpaypal123@gmail.com>
2024-03-23 14:45:48 +0200

    books

--- Controllers/BooksCotroller.cs
+++ Controllers/BooksCotroller.cs
@@ -15,7 +15,7 @@ namespace eTickets.Controllers
         public async Task<IActionResult> Index()
         {
             var allBooks = await _context.Books.ToListAsync();
-            return View();
+            return View(allBooks);
         }
     }
 }
