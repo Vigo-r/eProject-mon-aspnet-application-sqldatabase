ab54da353a66235e811a1d7a33d808f95166edfd
VIgo-r <prazno.kalin@gmail.com>
2024-03-23 17:58:37 +0200

    Massive Blunder Commit - Tried adding General Case Entity Base Repo for easier development of further addiotions.

--- Controllers/AuthorsController.cs
+++ Controllers/AuthorsController.cs
@@ -1,20 +1,110 @@
-﻿using eTickets.Data;
+﻿using System.Threading.Tasks;
+using eTickets.Data.Services;
+using eTickets.Models;
 using Microsoft.AspNetCore.Mvc;
-
+ 
 namespace eTickets.Controllers
 {
     public class AuthorsController : Controller
     {
-        private readonly AppDbContext _cotext;
-        public AuthorsController(AppDbContext context)
+        private readonly IAuthorsService _service;
+ 
+        public AuthorsController(IAuthorsService service)
         {
-            _cotext = context;
+            _service = service;
         }
-        public IActionResult Index()
+ 
+        public async Task<IActionResult> Index()
         {
-            var data = _cotext.Authors.ToList();
-
+            var data = await _service.GetAllAsync();
             return View(data);
+        }
+ 
+        // GET: Authors/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+ 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("FullName,ProfilePictureURL,Bio")] Author author)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(author);
+            }
+ 
+            await _service.AddAsync(author);
+            return RedirectToAction(nameof(Index));
+        }
+ 
+        // GET: Authors/Details/1
+        public async Task<IActionResult> Details(int id)
+        {
+            var authorDetails = await _service.GetByIdAsync(id);
+ 
+            if (authorDetails == null)
+            {
+                return NotFound();
+            }
+ 
+            return View(authorDetails);
+        }
+ 
+        // GET: Authors/Edit/1
+        public async Task<IActionResult> Edit(int id)
+        {
+            var authorDetails = await _service.GetByIdAsync(id);
+ 
+            if (authorDetails == null)
+            {
+                return NotFound();
+            }
+ 
+            return View(authorDetails);
+        }
+ 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,FullName,ProfilePictureURL,Bio")] Author author)
+        {
+            if (id != author.Id || !ModelState.IsValid)
+            {
+                return View(author);
+            }
+ 
+            await _service.UpdateAsync(id, author);
+            return RedirectToAction(nameof(Index));
+        }
+ 
+        // GET: Authors/Delete/1
+        public async Task<IActionResult> Delete(int id)
+        {
+            var authorDetails = await _service.GetByIdAsync(id);
+ 
+            if (authorDetails == null)
+            {
+                return NotFound();
+            }
+ 
+            return View(authorDetails);
+        }
+ 
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var authorDetails = await _service.GetByIdAsync(id);
+ 
+            if (authorDetails == null)
+            {
+                return NotFound();
+            }
+ 
+            await _service.DeleteAsync(id);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
+ 
--- Data/Services/AuthorsService.cs
+++ Data/Services/AuthorsService.cs
@@ -1,57 +1,13 @@
-﻿using eTickets.Models;
+﻿using eTickets.Data.Base;
+using eTickets.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace eTickets.Data.Services
 {
-    public class AuthorsService : IAuthorsService
+    public class AuthorsService : EntityBaseRepository<Author>, IAuthorsService
     {
-        public void Add(Author author)
-        {
-            throw new NotImplementedException();
-        }
 
-        public Task AddAsync(Author entity)
-        {
-            throw new NotImplementedException();
-        }
+        public AuthorsService(AppDbContext context) : base (context) { }
 
-        public void Delete(int id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task DeleteAsync(int id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IEnumerable<Author> GetAll()
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<IEnumerable<Author>> GetAllAsync()
-        {
-            throw new NotImplementedException();
-        }
-
-        public Author GetById(int id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<Author> GetByIdAsync(int id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Author Update(int id, Author newAuthor)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task UpdateAsync(int id, Author entity)
-        {
-            throw new NotImplementedException();
-        }
     }
 }
